5e32eb276820d314de01e33a1c39bbb3da1c1592
Evan Louie <evan.louie@microsoft.com>
2018-05-20 00:33:44 -0700

    namespace cleanup

--- src/name_generator/core.clj
+++ src/name_generator/core.clj
@@ -1,4 +1,5 @@
 (ns name-generator.core
+  (:require [clojure.string :as s])
   (:gen-class))
 
 (def read-file (memoize
@@ -7,19 +8,19 @@
 
 (defn get-last-names []
   (let [census-string (read-file "Names_2010Census.csv")
-        lines (clojure.string/split-lines census-string)
-        keys (map #(keyword %) (clojure.string/split (first lines) #","))
-        values (map #(clojure.string/split % #",") (rest lines))
+        lines (s/split-lines census-string)
+        keys (map #(keyword %) (s/split (first lines) #","))
+        values (map #(s/split % #",") (rest lines))
         census (map #(zipmap keys %) values)
-        last-names (map #(clojure.string/capitalize (get % :name)) census)]
+        last-names (map #(s/capitalize (get % :name)) census)]
     last-names))
 
 (defn get-first-names []
   (letfn [(get-name-from-line [line]
-            (clojure.string/capitalize (first (clojure.string/split line #"\s"))))
+            (s/capitalize (first (s/split line #"\s"))))
           (get-names-from-string [file-string]
                                  (map get-name-from-line
-                                      (clojure.string/split-lines file-string)))]
+                                      (s/split-lines file-string)))]
     (let [male-names-string (read-file "dist.male.first.txt")
           female-names-string (read-file "dist.female.first.txt")
           male-names (get-names-from-string male-names-string)
@@ -29,7 +30,7 @@
 (defn generate-name []
   (let [first-name (rand-nth (get-first-names))
         last-name (rand-nth (get-last-names))]
-    (clojure.string/join " " [first-name last-name])))
+    (s/join " " [first-name last-name])))
 
 (defn -main
   "I don't do a whole lot ... yet."
