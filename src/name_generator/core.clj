5972019cd654c88bf08cfa286510feedfdcc988c
Evan Louie <evan.louie@microsoft.com>
2018-05-28 17:13:14 -0700

    refactor into separate ns

--- src/name_generator/core.clj
+++ src/name_generator/core.clj
@@ -1,55 +1,20 @@
 (ns name-generator.core
-  (:require [clojure.string :as string])
+  (:require [name-generator.generator :refer [name-sequence]]
+            [clojure.string :as string])
+  (:import [name_generator.generator Name])
   (:gen-class))
-
-(defrecord Name [^String fname ^String lname])
-
-(def read-resource
-  ^{:private true
-    :doc "Read a file from project resources directory and return as string; Result is memoized"}
-  (memoize
-   (fn [filename]
-     (slurp (clojure.java.io/resource filename)))))
-
-(defn- get-last-names
-  "Parse the 2010 Census CSV and return a distinct sequence of last names"
-  []
-  (let [census-string (read-resource "Names_2010Census.csv")
-        lines (string/split-lines census-string)
-        keys (map #(keyword %) (string/split (first lines) #","))
-        values (map #(string/split % #",") (rest lines))
-        census (map #(zipmap keys %) values)
-        last-names (map #(string/capitalize (get % :name)) census)]
-    (distinct last-names)))
-
-(defn- get-first-names
-  "Parse the 1990 male and female first names files and return a distinct sequence"
-  []
-  (letfn [(get-name-from-line [line]
-            (string/capitalize (first (string/split line #"\s"))))
-          (get-names-from-string [file-string]
-                                 (map get-name-from-line
-                                      (string/split-lines file-string)))]
-    (let [male-names-string (read-resource "dist.male.first.txt")
-          female-names-string (read-resource "dist.female.first.txt")
-          male-names (get-names-from-string male-names-string)
-          female-names (get-names-from-string female-names-string)]
-      (distinct (concat male-names female-names)))))
-
-(defn generate-name
-  "Generate a random Name"
-  ^Name
-  []
-  (let [first-name (rand-nth (get-first-names))
-        last-name (rand-nth (get-last-names))]
-    (Name. first-name last-name)))
 
 (defn -main
   "I generate a random name"
   [& args]
-  (let [name (generate-name)
-        fname (:fname name)
-        lname (:lname name)
-        full-name (str fname " " lname)]
-    (println full-name)))
-
+  (do
+    (println "How many names should I generate?")
+    (def n (read-string (read-line)))
+    (def names (take n (name-sequence)))
+    (->> names
+         (map (fn [^Name name]
+                (let [full-name (str (:fname name) " " (:lname name))]
+                  (do (println full-name)
+                      full-name))))
+         (doall))
+    (shutdown-agents)))
---
+++ src/name_generator/generator.clj
@@ -0,0 +1,60 @@
+(ns name-generator.generator
+  (:require [clojure.string :as string]))
+
+(defrecord Name [^String fname ^String lname])
+
+(defn- read-resource
+  "Read a file from project resources directory and return as string."
+  ^String
+  [^String filename]
+  (slurp (clojure.java.io/resource filename)))
+
+(defn- get-last-names
+  "Parse the 2010 Census CSV and return a distinct sequence of last names."
+  ^clojure.lang.LazySeq
+  []
+  (let [census-string (read-resource "Names_2010Census.csv")
+        lines (string/split-lines census-string)
+        keys (map #(keyword %) (string/split (first lines) #","))
+        values (map #(string/split % #",") (rest lines))
+        census (map #(zipmap keys %) values)
+        last-names (map #(string/capitalize (get % :name)) census)]
+    (distinct last-names)))
+
+(defn- get-first-names
+  "Parse the 1990 male and female first names files and return a distinct sequence."
+  ^clojure.lang.LazySeq
+  []
+  (letfn [(get-name-from-line [^String line]
+            (string/capitalize (first (string/split line #"\s"))))
+          (get-names-from-string [^String file-string]
+                                 (map get-name-from-line
+                                      (string/split-lines file-string)))]
+    (let [names-strings (pmap read-resource ["dist.male.first.txt" "dist.female.first.txt"])
+          names (flatten (pmap get-names-from-string names-strings))]
+      (distinct names))))
+
+(def ^:private ^clojure.lang.LazySeq
+  first-names
+  "Memoized list of male and female first names."
+  (memoize (fn [] (get-first-names))))
+
+(def ^:private ^clojure.lang.LazySeq
+  last-names
+  "Memoized list of last names."
+  (memoize (fn [] (get-last-names))))
+
+(defn generate-name
+  "Generate a random Name"
+  ^Name
+  []
+  (let [first-name (future (rand-nth (first-names)))
+        last-name (future (rand-nth (last-names)))]
+    (Name. @first-name @last-name)))
+
+(defn name-sequence
+  "Lazy list of Names"
+  ^clojure.lang.LazySeq
+  []
+  (lazy-seq (cons (generate-name) (name-sequence))))
+
